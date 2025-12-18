555a6c2ccfff9afaff8c5b1e7adb468a5e518acd
ichizawa <ryonuzuke@gmail.com>
2025-12-18 14:27:20 +0800

    fixed ui problem

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,4 @@
 import './bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import jQuery from 'jquery';
 window.$ = jQuery;
--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    {{--
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mediaonetix</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -12,20 +13,17 @@
 </head>
 
 <body>
-    {{-- @if (Route::is('admin.*'))
-    @include('components.navbar')
-    @include('components.sidebar')
-    @endif --}}
-    <x-navbar />
-    <x-sidebar />
-    @yield('content')
-    <livewire:scripts />
+    <div>
+        <x-navbar />
+        <x-sidebar />
+        @yield('content')
+        <livewire:scripts />
+    </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
     <script src="{{ asset('js/login.js') }}"></script>
     <script src="{{ asset('js/sidebar.js') }}"></script>
-
 </body>
 
 </html>
