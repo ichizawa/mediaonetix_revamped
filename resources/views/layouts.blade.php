ddcecd0a3ae9e06026a8e1058194eb4536cffbc8
ichizawa <ryonuzuke@gmail.com>
2026-01-06 17:31:48 +0800

    esaesaesa

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mediaonetix</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/bootstrap.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <livewire:styles />
 </head>
 
